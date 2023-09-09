af7f6a7af13e18f37509d9e1a1f581ddec2d2fe1
Jefferson <jeffersondesouzacordel@gmail.com>
2023-09-09 17:50:58 -0300

    update docker summary

---
+++ projects/project-1/school-backend/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 8080;
+
+app.use(bodyParser());
+
+app.get("/", (_req, res) => {
+  res.json({
+    message: "welcome",
+  });
+});
+
+app.get("/classes", (_req, res) => {
+  res.json([
+    {
+      id: "1",
+      name: "Begin 1",
+    },
+    {
+      id: "2",
+      name: "Begin 2",
+    },
+    {
+      id: "3",
+      name: "Upper 1",
+    },
+    {
+      id: "3",
+      name: "Upper 2",
+    },
+  ]);
+});
+
+app.listen(port, () => {
+  console.log(`App ruining in http://localhost:${port}`);
+});
