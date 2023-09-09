8b1eea5e12590bee9d3fcfcde090f4193588eb76
Jefferson <jeffersondesouzacordel@gmail.com>
2023-09-09 18:03:42 -0300

    add api call

--- projects/project-1/school-frontend/src/App.js
+++ projects/project-1/school-frontend/src/App.js
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
+const getData = async () => {
+  const data = await axios.get("http://localhost:8080/classes");
+  console.log(data);
+  return data;
+};
+
 function App() {
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
